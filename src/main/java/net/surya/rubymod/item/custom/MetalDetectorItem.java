d40a22947323a3d0d4c290c2a01819ba5f63e241
notnovayt <gb8040@outlook.com>
2024-06-25 18:29:15 -0300

    Almost finished the Metal Detector class!

---
+++ src/main/java/net/surya/rubymod/item/custom/MetalDetectorItem.java
@@ -0,0 +1,43 @@
+package net.surya.rubymod.item.custom;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+
+public class MetalDetectorItem extends Item {
+    public MetalDetectorItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        if(!context.getWorld().isClient()) {
+            BlockPos positionClicked = context.getBlockPos();
+            PlayerEntity player = context.getPlayer();
+            boolean foundBlock = false;
+
+            for(int i = 0; i <= positionClicked.getY() + 64; i++) {
+                BlockState state = context.getWorld().getBlockState(positionClicked.down(i));
+
+                if(isValuableBlock(state)) {
+                    outputValuableCoordinates(positionClicked.down(i), player, state.getBlock());
+                    foundBlock = true;
+
+                    break;
+                }
+
+            }
+
+        }
+
+        return ActionResult.PASS;
+    }
+
+    private boolean isValuableBlock(BlockState state) {
+        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+    }
+}
