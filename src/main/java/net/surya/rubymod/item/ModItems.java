171d6ddef15a4d03d42beafb2d089901ed42d1b1
notnovayt <gb8040@outlook.com>
2024-06-30 20:07:59 -0300

    Added the Ruby Staff to the ModItems class and made a 3D texture for it. Also added the json file for the 3D texture.

--- src/main/java/net/surya/rubymod/item/ModItems.java
+++ src/main/java/net/surya/rubymod/item/ModItems.java
@@ -25,6 +25,9 @@ public class ModItems {
     public static final Item METAL_DETECTOR = registerItem("metal_detector",
             new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
 
+    public static final Item RUBY_STAFF = registerItem("ruby_staff",
+            new Item(new FabricItemSettings().maxCount(1)));
+
     private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
         entries.add(RAW_RUBY);
