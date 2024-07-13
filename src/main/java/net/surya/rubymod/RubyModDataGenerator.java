34635cbdd1c1e087b61c631da69585043630ff30
notnovayt <gb8040@outlook.com>
2024-07-13 19:02:56 -0300

    Made the poitagprovider class and added it to the data generator.

--- src/main/java/net/surya/rubymod/RubyModDataGenerator.java
+++ src/main/java/net/surya/rubymod/RubyModDataGenerator.java
@@ -14,5 +14,6 @@ public class RubyModDataGenerator implements DataGeneratorEntrypoint {
 		pack.addProvider(ModLootTableProvider::new);
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
+		pack.addProvider(ModPoiTagProvider::new);
 	}
 }
